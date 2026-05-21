18c7addd86b61334bd11098e389251a7a00d54f1
[]rlando (Y0i) <oryees27@gmail.com>
2026-05-21 11:55:18 -0500

    New Activity
    Methods and returns
    
    Co-Authored-By: YoiTM <oyepese@ut.edu.co>

--- Modules/Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Modules/Guided-project-branching-looping-CSharp-main/GuidedProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc2e2e9f9f9eccbcc117095059c1a47ee709ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
